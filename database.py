6e7a6ad4125588c0f720707d6086d52e854fec80
dhinesh123-ai <dhineshprofenaa123@gmail.com>
2026-06-13 23:55:59 +0530

    Change DATABASE_URL to use specific PostgreSQL string
    Updated DATABASE_URL to a specific PostgreSQL connection string.

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("postgresql://dhinesh:kxZuocCBPvRiLF7juAuv8Mw5BBFPLOht@dpg-d8mpta7lk1mc7390aq3g-a.singapore-postgres.render.com/tododb_j0ds")
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False)
 Base = declarative_base()
